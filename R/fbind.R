2bebd6c8ca627e08ce341bd2bbf5f92ff2e3a516
Dennis Malandro <dennismalandro@gmail.com>
2021-09-23 11:40:45 -0400

    Add tests and fcount

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,15 @@
+#' Concatenate two factors
+#'
+#' Create a new factor from two factors whose levels are the union of the two
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
